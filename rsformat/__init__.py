95526d946681ef0a43f5b7aa1fa9bd65c423373a
Chris O'Connor <45886288+cdoconno@users.noreply.github.com>
2019-03-23 04:25:55 -0500

    Change from rformat to rsformat
    Signed-off-by: Chris O'Connor <45886288+cdoconno@users.noreply.github.com>

---
+++ rsformat/__init__.py
@@ -0,0 +1,11 @@
+"""
+rsformat is a helper to format and manage datasets.
+The goal is to allow configuration based filtering to provide
+a layer of abstraction from queried result sets if those sets
+need small changes
+"""
+import os
+from . import resultset
+
+name = "rsformat"
+__version__ = '0.0.1'
