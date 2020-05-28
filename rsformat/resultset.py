5100c6ee423cc556ddb42786db681d09bb2e9744
Chris O'Connor <45886288+cdoconno@users.noreply.github.com>
2020-05-27 22:37:44 -0500

    Added formatters and order map processor

--- rsformat/resultset.py
+++ rsformat/resultset.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+import datetime
 from collections import namedtuple, OrderedDict
 import types
 
@@ -55,9 +56,15 @@ class ResultSet(object):
     """
     def __init__(self, results, headers=None, order_map=None):
         if order_map:
-            self.order_map = self._sort_order_map(order_map)
+            om = {}
+            for k in order_map.keys():
+                col = order_map.get('col')
+                om[k] = col if col is not None else order_map[k].get('alias')
+            self.order_map = self._sort_order_map(om)
+            self.order_map_raw = order_map
         else:
             self.order_map = order_map
+            self.order_map_raw = order_map
         self.headers, self.header_source = self._manageheaders(headers)
         self.rowdef = namedtuple("RsRow", self.headers, rename=True)
         self.generate_rows = None
@@ -95,11 +102,12 @@ class ResultSet(object):
         Add a single row to resultset
         """
         try:
-            self.rows.append(Row(row, self.rowdef))
+            self.rows.append(Row(row, self.rowdef, self.order_map_raw))
             self.row_count += 1
-        except Exception:
+        except Exception as e:
             self.errors.append(row)
             self.error_count += 1
+            print("Error adding row: %s" % e)
 
     def _manageheaders(self, headers):
         """
@@ -149,14 +157,20 @@ class Row(object):
 
     __slots__ = ('data')
 
-    def __init__(self, values, rowdef=None):
-        self.data = self._normalize_row(values, rowdef)
+    def __init__(self, values, rowdef=None, order_map=None):
+        self.data = self._normalize_row(values, rowdef, order_map)
 
     def rowdef(self):
         return self.data._fields
 
+    def get(self, key):
+        return self.data[self.data._fields.index(key)]
+
+    def as_csv(self):
+        return "%s," % ",".join(prop if prop else "" for prop in self.data)
+
     @staticmethod
-    def _normalize_row(values, rowdef):
+    def _normalize_row(values, rowdef, order_map):
         """
         return a named tuple of values if provided bounded iterable dict, tuple, etc
         """
@@ -167,10 +181,119 @@ class Row(object):
             return rowdef._make(values)
         elif tp == dict:
             # filtered is a new dict only the named fields of the rowdef
-            filtered = {k: values.get(k, None) for k in rowdef._fields}
+            filtered = {k: format(values.get(k, None), order_map[i].get('format', [])) for i, k in enumerate(rowdef._fields)}
             return rowdef(**filtered)
         else:
             raise TypeError("must provide values as bounded iterable for new Row")
+
+
+def format(value, formatters):
+    """Runs a value through a formatter pipeline"""
+    # print("formatting value: %s with formatters: %s" % (value, formatters))
+    for fmt in formatters:
+        type = fmt.get('type')
+        name = "format_%s" % (type)
+        func = None
+        try:
+            func = getattr(sys.modules[__name__], name)
+        except AttributeError:
+            print("Function does not exist: %s" % name)
+        if isinstance(func, types.FunctionType):
+            value = func(value, fmt)
+    return value
+
+
+def format_value(value, props):
+    val = props.get('value')
+    return val
+
+
+def format_conversion(value, props):
+    tp = props.get('convert')
+    if value in(None, ''):
+        return value
+    if tp == "int":
+        return int(value)
+    elif tp == "float":
+        return float(value)
+    elif tp == "str":
+        return "%s" % value
+    else:
+        raise ValueError("Must provide type as int float or str. provided: %s" % tp)
+
+
+def format_date(value, props):
+    """Date formatter"""
+    if value in(None, ''):
+        return value
+    fmt_from = props.get('from', None)
+    fmt_to = props.get('to', None)
+    if fmt_from is None:
+        raise ValueError("must provide from format with date formatting")
+    if fmt_to is None:
+        raise ValueError("must provide to format with date formatting")
+    try:
+        new_date = datetime.datetime.strptime(value, fmt_from)
+        return new_date.strftime(fmt_to)
+    except Exception:
+        raise ValueError("problem formatting date with strptime for value %s and format %s -> %s" % (value, fmt_from, fmt_to))
+
+
+def format_mapping(value, props):
+    """Map formatter"""
+    maps = props.get('maps')
+    default = props.get('default')
+    for map in maps:
+        map_from = map.get('from')
+        map_to = map.get('to')
+        if value == map_from:
+            return map_to
+    if default is not None:
+        return default
+    return value
+
+
+def format_string(value, props):
+    """String formatter"""
+    func = props.get('func')
+    if func is None:
+        raise ValueError("must provide a func for string format")
+
+    return perform_format_string(value, func, props)
+
+
+def perform_format_string(value, string_func, props):
+    name = "_format_string_%s" % (string_func)
+    func = None
+    try:
+        func = getattr(sys.modules[__name__], name)
+    except AttributeError:
+        print("Function does not exist: %s" % name)
+    if isinstance(func, types.FunctionType):
+        return func(value, props)
+
+
+def _format_string_format(value, props):
+    tmpl = props.get('tmpl')
+    if tmpl is None:
+        raise ValueError("must provide a tmpl property for string format")
+    if value in (None, ''):
+        return value
+    return tmpl.format(value)
+
+
+def _format_string_upper(value, props):
+    return value.upper()
+
+
+def _format_string_lower(value, props):
+    return value.lower()
+
+
+def _format_string_replace(value, props):
+    find = props.get('find')
+    replace = props.get('replace')
+    return value.replace(find, replace)
 
 
 if __name__ == "__main__":
